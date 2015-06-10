d4cccdd593322c13913d92f105ab2eb12ab4a9b7
didacs <didac.santesmasses@crg.eu>
2015-06-10 21:11:54 +0200

    initial commit

---
+++ R/ggtex.R
@@ -0,0 +1,19 @@
+#' Loads GTEx metadata
+#' 
+#' @param SubjectPhenotypes path to SubjectPhenotypes file.
+#' @param SubjectSampleMapping path to SubjectSampleMapping file.
+#' @param SampleAttributes path to SampleAttributes file.
+#' @return data.frame with merged metadata
+
+load_metadata <- function(
+  SubjectPhenotypes,
+  SubjectSampleMapping,
+  SampleAttributes
+  ) {
+  SubjectPhenotypes <- read.delim(SubjectPhenotypes, fill=F)
+  SubjectSampleMapping <- read.delim(SubjectSampleMapping, fill=F)
+  SampleAttributes <- read.delim(SampleAttributes, fill=F)
+  metadata <- merge(SubjectPhenotypes,SubjectSampleMapping, by='SUBJID')
+  metadata <- merge(metadata,SampleAttributes, by='SAMPID')
+  metadata
+}
